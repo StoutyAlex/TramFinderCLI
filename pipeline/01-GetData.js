72c5cc118d731386b71309771fe26744d8c731aa
Alex Stout <alexstout009@hotmail.com>
2019-04-01 15:15:10 +0100

    Removed old built in processing

--- pipeline/01-GetData.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-module.exports = async () => {
-  return await axios.get('https://api.tfgm.com/odata/Metrolinks', {
-    headers: {
-      'Ocp-Apim-Subscription-Key': process.env.METRO_API, 
-    }
-  });
-};
