b1224d9251a308fe784f4016a2bff24898db7361
Alex Stout <alexstout009@hotmail.com>
2019-04-01 15:15:27 +0100

    Added simple call to external lambda

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
 require('dotenv').config();
-const pipeline = require('./pipeline');
+const axios = require('axios');
 
-pipeline('piccadilly gardens').then(data => console.log(data));
+const stationName = 'ladywell';
+
+const getData = async () => {
+  return await axios.get('https://0snmtmpnm4.execute-api.eu-west-1.amazonaws.com/default/metrolink', {
+    params: {
+      station: stationName,
+    }
+  });
+};
+
+const printData = (data) => {
+  console.log(`Times for ${stationName}`);
+  console.log('==============');
+  console.log('Incoming trams: ');
+  
+  data.incoming.forEach((tram, i) => {
+    tram.wait.forEach((time, i) => {
+      console.log(`${tram.destination} in ${time} minutes`);
+    });
+  });
+
+  console.log('==============');
+  console.log('Outgoing trams: ');
+  
+  data.outgoing.forEach((tram, i) => {
+    tram.wait.forEach((time, i) => {
+      console.log(`${tram.destination} in ${time} minutes`);
+    });
+  });
+
+}
+
+getData()
+  .then(res => printData(res.data.data))
+  .catch(err => console.log(err.response.data));
